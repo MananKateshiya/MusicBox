64ecf8abe8d25b0eebfc6b5547d2f1014de0f3db
manan <manankateshiya@gmail.com>
2021-03-28 20:53:17 +0530

    Initian Commit

---
+++ js/shrink.js
@@ -0,0 +1,14 @@
+$(document).on("scroll", function () {
+
+    if ($(document).scrollTop() > 80) {
+        $(".primary-nav").addClass("shrink");
+    } else {
+        $(".primary-nav").removeClass("shrink");
+    }
+
+
+// $(document).ready(function(){
+
+// });
+
+});
