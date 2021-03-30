9e29f02665f7c49dd861685307f40903775fa3c0
manan <manankateshiya@gmail.com>
2021-03-30 18:51:19 +0530

    Shop page and some fixes

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+// $(".js-range-slider").ionRangeSlider();
+
+$(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 100,
+    max: 10000,
+    from: 200,
+    to: 5000,
+    grid: true,
+    prettify_enabled:true,
+});
